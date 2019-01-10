dde6ed624d735e5cd7724a7f582c2db418b04de9
zhenhua.zhu <zhenhzhu@126.com>
2019-01-10 10:46:01 +0800

    add datasource

--- deepmind/datasource.py
+++ deepmind/datasource.py
@@ -16,20 +16,21 @@ class DataSource(object):
         raise NotImplementedError()
 
 class CSVDataSource(DataSource):
-    def __init__(self, path):
+    def __init__(self, path, *, names):
         if not path:
             raise AttributeError()
         self.path = os.path.abspath(path)
+        self.names = names
 
     def read(self):
-        df = pd.read_csv(self.path, delim_whitespace=True, names=['fly', 'game', 'sweet', 'date'])
+        df = pd.read_csv(self.path, delim_whitespace=True, names=self.names)
         self.dataFrame = df
-        print('Read', self.path)
+        print('Read', self.dataFrame)
 
     def seperate(self):
         pass
 
 
 if __name__ == '__main__':
-    datasource = CSVDataSource('dataset/datingTestSet.txt')
+    datasource = CSVDataSource('dataset/datingTestSet.txt', names=['fly', 'game', 'sweet', 'date'])
     datasource.read()
